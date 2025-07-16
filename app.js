abf37421832fadae07bc64f29960ffcb13a13b1d
23048180-yongkeong <23048180@myrp.edu.sg>
2025-07-16 18:27:46 +0800

    Remove connection timeout delay and set connectTimeout for MySQL connection

--- app.js
+++ app.js
@@ -13,20 +13,17 @@ const connection = mysql.createConnection({
    host: 'db4free.net',
    user: 'busticketapp',
    password: 'busticketapp',
-   database: 'busticketapp',
-   connectTimeout: 10000, // 10 seconds
+   database: 'busticketapp'
 });
 
-setTimeout(() => {
-    connection.connect((err) => {
+connection.connect((err) => {
     if (err)
     {
         console.error('Error connecting to MySQL:', err)
         return;
     }
     console.log('Connected to MySQL database');
-    });
-}, 5000); // Wait for 5 seconds before connecting
+});
 
 const PORT = process.env.PORT || 3000;
 
